910568995cb1c28597d40630966bc414aabcde5d
PakaDopa <detols33@naver.com>
2025-04-25 02:08:18 +0900

    Feat. git add .

--- kbu_task1.sh
+++ kbu_task1.sh
@@ -13,15 +13,15 @@ export PYENV_ROOT="/root/.pyenv"
 git clone https://github.com/pyenv/pyenv.git $PYENV_ROOT
 
 # pyenv 환경 변수 및 초기화 - .bashrc에 추가
-echo 'export PYENV_ROOT="/root/.pyenv"' >> /root/.bashrc
-echo 'export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"' >> /root/.bashrc
-echo 'eval "$(pyenv init --path)"' >> /root/.bashrc
-echo 'eval "$(pyenv init -)"' >> /root/.bashrc
+# echo 'export PYENV_ROOT="/root/.pyenv"' >> /root/.bashrc
+# echo 'export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"' >> /root/.bashrc
+# echo 'eval "$(pyenv init --path)"' >> /root/.bashrc
+# echo 'eval "$(pyenv init -)"' >> /root/.bashrc
 
 # 현재 셸에서도 반영
-export PATH="/root/.pyenv/bin:/root/.pyenv/shims:$PATH"
-eval "$(/root/.pyenv/bin/pyenv init --path)"
-eval "$(/root/.pyenv/bin/pyenv init -)"
+# export PATH="/root/.pyenv/bin:/root/.pyenv/shims:$PATH"
+# eval "$(/root/.pyenv/bin/pyenv init --path)"
+# eval "$(/root/.pyenv/bin/pyenv init -)"
 
 # Python 설치
 pyenv install 3.12.0 -s
