2de46a641d2fd933774607425fb77359b7f5bdf5
PakaDopa <detols33@naver.com>
2025-04-25 01:46:00 +0900

    Feat. Update7

--- kbu_task1.sh
+++ kbu_task1.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 set -e
 
-# 필요한 의존성 설치 (리눅스 예시)
-sudo apt-get update
-sudo apt-get install make gcc ncurses-dev -y
+# 필수 패키지 설치
+apt-get update
+apt-get install -y make build-essential curl git zlib1g-dev \
+  libssl-dev libbz2-dev libreadline-dev libsqlite3-dev wget \
+  llvm libncursesw5-dev xz-utils tk-dev libxml2-dev \
+  libxmlsec1-dev libffi-dev liblzma-dev ca-certificates
 
-# pyenv 설치 스크립트 다운로드
-wget -O install.sh https://github.com/pyenv/pyenv-installer/raw/master/install.sh
+# pyenv 설치
+export PYENV_ROOT="/root/.pyenv"
+git clone https://github.com/pyenv/pyenv.git $PYENV_ROOT
 
-# pyenv 설치 스크립트 실행
-bash install.sh
+# pyenv 환경 변수 및 초기화 - .bashrc에 추가
+echo 'export PYENV_ROOT="/root/.pyenv"' >> /root/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"' >> /root/.bashrc
+echo 'eval "$(pyenv init --path)"' >> /root/.bashrc
+echo 'eval "$(pyenv init -)"' >> /root/.bashrc
 
-# pyenv 경로 추가 (예시)
-echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'export PYENV_VERSION="2.3.19"' >> ~/.bashrc
-echo 'export PYENV_ROOT_OLD="$HOME/.pyenv"' >> ~/.bashrc
-echo 'eval "$(pyenv init bash)"' >> ~/.bashrc
-
-# 설정 적용
-source ~/.bashrc
+# 현재 셸에서도 반영
+export PATH="/root/.pyenv/bin:/root/.pyenv/shims:$PATH"
+eval "$(/root/.pyenv/bin/pyenv init --path)"
+eval "$(/root/.pyenv/bin/pyenv init -)"
 
 # Python 설치
 pyenv install 3.12.0 -s
 pyenv global 3.12.0
 
-# 설치 확인 (채점기에서 따로 하겠지만 여기도 출력)
+# 확인
 pyenv -v
 python --version
