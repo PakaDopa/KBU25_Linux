573e00c17849598d9d7a8e5fdd996c3b0d3f1c8b
PakaDopa <detols33@naver.com>
2025-04-25 01:09:03 +0900

    Feat. Update2

--- kbu_task1.sh
+++ kbu_task1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e  # 중간에 실패하면 즉시 종료
 
-# 시스템 업데이트 및 필수 패키지 설치
+# 필수 패키지 설치
 apt-get update
 apt-get install -y make build-essential curl git zlib1g-dev \
   libssl-dev libbz2-dev libreadline-dev libsqlite3-dev wget \
@@ -11,18 +12,17 @@ apt-get install -y make build-essential curl git zlib1g-dev \
 export PYENV_ROOT="/root/.pyenv"
 git clone https://github.com/pyenv/pyenv.git $PYENV_ROOT
 
-# pyenv 경로 설정
+# 환경 변수 설정
 export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 
-# Python 3.12.0 설치
+# Python 설치
 pyenv install 3.12.0 -s
 
-# 전역 Python 버전 설정
+# 전역 설정
 pyenv global 3.12.0
 
-# 설치 결과 확인
+# 확인
 pyenv -v
 python --version
-
