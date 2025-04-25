39dcfefc82f4a381bfe6eab001917c7565f338c6
PakaDopa <detols33@naver.com>
2025-04-25 09:29:17 +0900

    Feat. git config --global user.email detols33@naver.com

--- kbu_task2.sh
+++ kbu_task2.sh
@@ -9,25 +9,26 @@ apt-get install -y make build-essential curl git zlib1g-dev \
   libxmlsec1-dev libffi-dev liblzma-dev ca-certificates
 
 # pyenv 설치
-export PYENV_ROOT="/root/.pyenv"
-git clone https://github.com/pyenv/pyenv.git $PYENV_ROOT
+export PYENV_ROOT="$HOME/.pyenv"
+git clone https://github.com/pyenv/pyenv.git "$PYENV_ROOT"
 
-# pyenv 환경 변수 및 초기화 - .bashrc에 추가
-# echo 'export PYENV_ROOT="/root/.pyenv"' >> /root/.bashrc
-# echo 'export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"' >> /root/.bashrc
-# echo 'eval "$(pyenv init --path)"' >> /root/.bashrc
-# echo 'eval "$(pyenv init -)"' >> /root/.bashrc
+# pyenv 환경변수 설정을 현재 사용자 환경파일에 추가
+if [ -w "$HOME/.bashrc" ]; then
+  echo 'export PYENV_ROOT="$HOME/.pyenv"' >> "$HOME/.bashrc"
+  echo 'export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"' >> "$HOME/.bashrc"
+  echo 'eval "$(pyenv init --path)"' >> "$HOME/.bashrc"
+  echo 'eval "$(pyenv init -)"' >> "$HOME/.bashrc"
+fi
 
-# 현재 셸에서도 반영
-# export PATH="/root/.pyenv/bin:/root/.pyenv/shims:$PATH"
-# eval "$(/root/.pyenv/bin/pyenv init --path)"
-# eval "$(/root/.pyenv/bin/pyenv init -)"
+# 현재 셸에서도 즉시 적용
+export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"
+eval "$("$PYENV_ROOT/bin/pyenv" init --path)"
+eval "$("$PYENV_ROOT/bin/pyenv" init -)"
 
-# Python 설치
+# Python 설치 및 전역 설정
 pyenv install 3.12.0 -s
 pyenv global 3.12.0
 
-# 확인
+# pyenv 및 Python 버전 확인
 pyenv -v
 python --version
-
